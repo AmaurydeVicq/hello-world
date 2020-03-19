b4cdbb792203271ddcb60798716b01189e57ebe4
AmaurydeVicq <Amaurydevicq@gmail.com>
2020-03-19 15:08:28 +0100

    Update new data

--- Pandemics&StockExchanges.R
+++ Pandemics&StockExchanges.R
@@ -67,7 +67,33 @@ GDPC2 <- GDPC %>%
   pivot_longer(-`country name`,names_to = "year", values_to = "GDP per Capita") %>%
   filter(year >= 1900) 
 GDPC2 <- GDPC2[-1,]
-               
+
+
+RealWage2 <- RealWage %>%
+  filter(`country name` == "Belgium" | `country name` == "France" | `country name` == "Netherlands" | `country name` == "Germany" | `country name` == "United States") %>%
+  pivot_longer(-`country name`,names_to = "year", values_to = "Real Wage") %>%
+  filter(year >= 1900)
+RealWage2 <- RealWage[-1,]  
+
+Life2 <- Life %>%
+  filter(`country name` == "Belgium" | `country name` == "France" | `country name` == "Netherlands" | `country name` == "Germany" | `country name` == "United States") %>%
+  pivot_longer(-`country name`,names_to = "year", values_to = "LifeEx") %>%
+  filter(year >= 1900)
+Life2 <- Life2[-1,]  
+
+BondYield2 <- BondYield %>%
+  filter(`country name` == "Belgium" | `country name` == "France" | `country name` == "Netherlands" | `country name` == "Germany" | `country name` == "United States") %>%
+  pivot_longer(-`country name`,names_to = "year", values_to = "Yield") %>%
+  filter(year >= 1900)
+BondYield2 <- BondYield2[-1,]
+
+Debt2 <- Debt %>%
+  filter(`country name` == "Belgium" | `country name` == "France" | `country name` == "Netherlands" | `country name` == "Germany" | `country name` == "United States") %>%
+  pivot_longer(-`country name`,names_to = "year", values_to = "Debt") %>%
+  filter(year >= 1900)
+Debt2 <- Debt2[-1] 
+
+
 # Plots
 All %>%
   filter(Age == 40) %>%
@@ -77,8 +103,14 @@ BENEFRUSA %>%
   filter(Age == 60) %>%
   filter(Year >= 1920) %>%
   ggplot(aes(x = Year, y = Total, color = country, group = country)) + geom_line() + facet_wrap(~ country)
-BENEFRUSA
 
 GDPC2 %>%
-  ggplot(aes(x = year, y = `GDP per Capita`, color = `country name`, group = `country name`)) + geom_line() + facet_wrap(~ `country name`) + scale_x_discrete(c1900,2000,100)
-BENEFRUS
+  ggplot(aes(x = year, y = `GDP per Capita`, color = `country name`, group = `country name`)) + geom_line() + facet_wrap(~ `country name`) 
+
+Life2 %>%
+  ggplot(aes(x = year, y = `LifeEx`, color = `country name`, group = `country name`)) + geom_line() + facet_wrap(~ `country name`)
+
+BondYield2 %>%
+  ggplot(aes(x = year, y = `Yield`, color = `country name`, group = `country name`)) + geom_line() + facet_wrap(~ `country name`)
+  
+
