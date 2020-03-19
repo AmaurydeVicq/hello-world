d4b35e184c663f188e5ba2e0813079cc7b72f0f0
basm92 <basmachielsen@live.nl>
2020-03-19 13:16:52 +0100

    New update pandemics and stocks

--- Pandemics&StockExchanges.R
+++ Pandemics&StockExchanges.R
@@ -5,14 +5,15 @@ library(dplyr)
 library(readxl)
 library(xlsx)
 library(tidyr)
+library(ggplot2)
 
 # Load in data
 
-BE_DR <-  read.csv("C:/Users/Amaur/Desktop/hello-world/Data/BE_DeathRate.txt", sep="")
-NL_DR <- read_excel("C:/Users/Amaur/Desktop/hello-world/Data/NL_DR.xlsx")
-FR_DR <-  read.csv("C:/Users/Amaur/Desktop/hello-world/Data/FR_DeathRate.txt", sep="")
-DE_DR <-  read.csv("C:/Users/Amaur/Desktop/hello-world/Data/DE_DeathRate.txt", sep="")
-USA_DR <-  read.csv("C:/Users/Amaur/Desktop/hello-world/Data/USA_DeathRate.txt", sep="")
+BE_DR <-  read.csv("./Data/BE_DeathRate.txt", sep="")
+NL_DR <- read_excel("./Data/NL_DR.xlsx")
+FR_DR <-  read.csv("./Data/FR_DeathRate.txt", sep="")
+DE_DR <-  read.csv("./Data/DE_DeathRate.txt", sep="")
+USA_DR <-  read.csv("./Data/USA_DeathRate.txt", sep="")
 
 # Amend data
 BE_DR$Year <- as.integer(BE_DR$Year) 
@@ -21,11 +22,35 @@ FR_DR$Year <- as.integer(FR_DR$Year)
 DE_DR$Year <- as.integer(DE_DR$Year)
 USA_DR$Year <- as.integer(DE_DR$Year)
 
-BE <- BE_DR %>% select(1,2,5)
-NL <- NL_DR %>% select(1,2,5)
-FR <- FR_DR %>% select(1,2,5)
-DE <- DE_DR %>% select(1,2,5)
-USA <- USA_DR %>% select(1,2,5)
+BE <- BE_DR %>% 
+  select(1,2,5) %>%
+  mutate(country = "Belgium", Age = as.numeric(as.character(Age)), Total = as.numeric(as.character(Total)))
+
+NL <- NL_DR %>%
+  select(1,2,5) %>%
+  mutate(country = "Netherlands", Age = as.numeric(as.character(Age)), Total = as.numeric(as.character(Total)))
+
+FR <- FR_DR %>% 
+  select(1,2,5) %>%
+  mutate(country = "France", Age = as.numeric(as.character(Age)), Total = as.numeric(as.character(Total)))
+
+DE <- DE_DR %>%
+  select(1,2,5) %>%
+  mutate(country = "Germany", Age = as.numeric(as.character(Age)), Total = as.numeric(as.character(Total)))
+
+USA <- USA_DR %>% 
+  select(1,2,5) %>%
+  mutate(country = "United States", Age = as.numeric(as.character(Age)), Total = as.numeric(as.character(Total)))
+
+
+
+All <- rbind(BE,NL,FR,DE,USA)
+
+#Graphique 
+
+All %>%
+  filter(Age == 40) %>%
+  ggplot(aes(x = Year, y = Total, color = country, group = country)) + geom_line()
 
 # Join datasets
 BENL <- full_join(BE,NL, by = "Year")
